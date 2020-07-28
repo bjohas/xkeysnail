69c9c4e10d0835c756ade1d249ee7daac7331706
pat.frew <patrick.frew@gmail.com>
2020-07-28 01:30:53 +0100

    Testing tearDown between tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,9 +1,10 @@
-import unittest
-import os
+import unittest, os, sys
+from importlib import reload
 from unittest import mock
 from unittest.mock import MagicMock, call
+
+from evdev import uinput, ecodes
 from xkeysnail.key import Action, Combo, Key, Modifier
-from evdev import uinput
 with mock.patch('evdev.uinput.UInput') as mock_uinput:
     from xkeysnail.transform import on_event, define_modmap
 
@@ -17,39 +18,66 @@ def evt(code, value = Action.PRESS):
         'value': value
     })
 
-mock_uinput.syn = MagicMock();
-mock_uinput.write_event = MagicMock();
-mock_uinput.write = MagicMock();
+mock_uinput.syn = MagicMock()
+mock_uinput.write_event = MagicMock()
+mock_uinput.write = MagicMock()
+
+# clear the empty call()
+mock_uinput.reset_mock()
 
 # to test a complete config
 # eval_file(os.path.dirname(os.path.abspath(__file__)) + '/config.py')
 
-class TestSetup(unittest.TestCase):
+# todo: support/mock device
+# todo: support/mock xlib.display
+# todo: helpers for input and expectations
+# todo: define unit tests for standard functionality (like this test)
+# todo: test for regression test against a complete config
+# https://docs.python.org/3/library/unittest.mock-examples.html
 
-    def test_test(self):
+def reload_modules():
+    # :/ reload transform module
+    reload(sys.modules[on_event.__module__]);
 
+class TestTearDown(unittest.TestCase):
+
+    def tearDown(self):
+        reload_modules();
         mock_uinput.reset_mock()
 
-        # todo: support/mock device
-        # todo: support/mock xlib.display
-        # todo: helpers for input and expectations
-        # https://docs.python.org/3/library/unittest.mock-examples.html
+    def test_create_modmap(self):
 
         define_modmap({
-            Key.CAPSLOCK: Key.LEFT_CTRL
+            Key.TAB: Key.LEFT_CTRL
         })
 
         on_event(
-            evt(Key.CAPSLOCK),
+            evt(Key.TAB),
             'DEVICE TODO',
             True
         )
 
         self.assertEqual(mock_uinput.mock_calls, [
-            call().write(1, Key.LEFT_CTRL, Action.PRESS),
+            call().write(ecodes.EV_KEY, Key.LEFT_CTRL, Action.PRESS),
             call().syn()
         ])
-#       print(mock_uinput.mock_calls)
+
+    def test_previous_modmap_should_no_longer_exist(self):
+
+        # if tearDown is working correctly:
+        #   - the modmap defined in first test should no longer exist
+        #   - mock_calls should be empty again at start of this test
+
+        on_event(
+            evt(Key.TAB),
+            'DEVICE TODO',
+            True
+        )
+
+        self.assertEqual(mock_uinput.mock_calls, [
+            call().write(ecodes.EV_KEY, Key.TAB, Action.PRESS),
+            call().syn()
+        ])
 
 if __name__ == '__main__':
     unittest.main()
