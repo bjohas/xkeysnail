02b84e1bb68e0d8297402300a98b488f5111832b
pat.frew <patrick.frew@gmail.com>
2020-08-01 18:12:45 +0100

    Add conditional modmap testcase

--- tests/test_modmap.py
+++ tests/test_modmap.py
@@ -36,5 +36,45 @@ class TestModmap(unittest.TestCase):
 
         self.assertEqual(received, expected)
 
+    @mock.patch('xkeysnail.transform.get_active_window_wm_class')
+    def test_conditional_modmap(self, get_wm_class):
+
+        define_conditional_modmap(re.compile(r'test_window_class'), {
+           Key.ENTER: Key.LEFT_CTRL,
+        })
+
+        # test with correct wm_class
+
+        get_wm_class.return_value = 'test_window_class'
+
+        keys = helpers.send_keys(on_event, [
+            { 'k': Key.ENTER },
+        ])
+
+        expected = helpers.get_call_keys([
+            { 'k': Key.LEFT_CTRL },
+        ])
+
+        received = helpers.collapse_extra_release(uinput.get_mock_calls())
+
+        self.assertEqual(received, expected)
+
+        # test without correct wm_class
+
+        uinput.reset_mock()
+        get_wm_class.return_value = ''
+
+        keys = helpers.send_keys(on_event, [
+            { 'k': Key.ENTER },
+        ])
+
+        expected = helpers.get_call_keys([
+            { 'k': Key.ENTER },
+        ])
+
+        received = helpers.collapse_extra_release(uinput.get_mock_calls())
+
+        self.assertEqual(received, expected)
+
 if __name__ == '__main__':
     unittest.main()
